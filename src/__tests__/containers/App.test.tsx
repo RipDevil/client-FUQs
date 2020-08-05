4849d0b41a10e85233dad6eb77ef1fae2fd4aafc
Mark Naberukhin <mna@startext.ru>
2020-08-05 15:44:49 +0700

    basic client template

---
+++ src/__tests__/containers/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from 'containers/app/App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/FUQs/i);
+  expect(linkElement).toBeInTheDocument();
+});
