50429ed3642cf049701172153689ad56bcc36f55
RipDevil <tiberi64@yandex.ru>
2020-08-26 15:16:47 +0700

    :dvd: root component and layout change

--- src/containers/layout/Layout.tsx
+++ src/containers/layout/Layout.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Row, Col } from 'antd';
+import { Row } from 'antd';
 
 const StyledRow = styled(Row)`
   margin-top: 50vh;
 `;
 
-const Layout: React.FC = ({ children }) => {
+export interface LayoutProps {
+  span?: number;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, span = 2 }) => {
   return (
     <StyledRow align='middle' justify='center'>
-      <Col span={2}>{children}</Col>
+      {children}
     </StyledRow>
   );
 };
---
+++ src/containers/root/Root.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Layout from 'containers/layout/Layout';
+import { Row, Col } from 'antd';
+
+const Root: React.FC = () => {
+  return (
+    <Layout span={24}>
+      Test <h1>Test</h1>
+    </Layout>
+  );
+};
+
+export default Root;
