13210078207ae031ad7d655d76a0ebdf53181e88
RipDevil <tiberi64@yandex.ru>
2020-08-26 16:25:35 +0700

    :dvd: fetch fuq

--- src/containers/root/Root.tsx
+++ src/containers/root/Root.tsx
@@ -1,11 +1,36 @@
 import * as React from 'react';
+import axios, { AxiosError } from 'axios';
+import { useStore } from 'effector-react';
+import { useQuery } from 'react-query';
+// import { Row, Col } from 'antd';
+
+import $config from 'stores/config';
 import Layout from 'containers/layout/Layout';
-import { Row, Col } from 'antd';
+
+export interface FuqProps {
+  _id: string;
+  likes: number;
+  _lastEditor: string;
+  pending: boolean;
+  title: string;
+  text: string;
+  crdate: string;
+}
 
 const Root: React.FC = () => {
+  const {
+    api: { fuq },
+    server,
+  } = useStore($config);
+
+  const { data, isSuccess, isError, error } = useQuery('get a single FUQ', () =>
+    axios.get(`${server}${fuq?.get}`).then((res): FuqProps => res.data)
+  );
+
   return (
     <Layout span={24}>
-      Test <h1>Test</h1>
+      {isError && error && error.message}
+      {isSuccess && data?.text}
     </Layout>
   );
 };
