4382aaa51ddd482b83dc0648912cbe6a0864a04f
RipDevil <tiberi64@yandex.ru>
2020-08-26 15:16:07 +0700

    :dvd: implemented the root component

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Switch, Route, HashRouter } from 'react-router-dom';
 import Spinner from 'components/common/Spinner';
 
+const Root = React.lazy(() => import('containers/root/Root'));
 const Fuq = React.lazy(() => import('containers/fuq/Fuq'));
 const Admin = React.lazy(() => import('containers/admin/Admin'));
 const PageNotFound = React.lazy(() => import('containers/404/404'));
@@ -9,11 +10,11 @@ const PageNotFound = React.lazy(() => import('containers/404/404'));
 const App: React.FC = () => {
   return (
     // TODO: implement transition spinner
-    <React.Suspense fallback={<Spinner />}>
+    <React.Suspense fallback={<Spinner text='Loading the page' />}>
       {/* HashRouter - for git pages */}
       <HashRouter>
         <Switch>
-          <Route exact path='/' component={() => <b>/</b>} />
+          <Route exact path='/' component={Root} />
           <Route exact path='/fuq/:id' component={Fuq} />
           <Route exact path='/badmin' component={Admin} />
           <Route exact path='*' component={PageNotFound} />
