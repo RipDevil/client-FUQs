4244fe6fbf80400176d1e8875f991d647049dbc4
RipDevil <tiberi64@yandex.ru>
2020-08-26 12:33:07 +0700

    :dvd: lazyload

--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Fuq from 'containers/fuq/Fuq';
-import Admin from 'containers/admin/Admin';
-import PageNotFound from 'containers/404/404';
+import Spinner from 'components/common/Spinner';
+
+const Fuq = React.lazy(() => import('containers/fuq/Fuq'));
+const Admin = React.lazy(() => import('containers/admin/Admin'));
+const PageNotFound = React.lazy(() => import('containers/404/404'));
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={() => <b>/</b>} />
-        <Route exact path='/fuq/:id' component={Fuq} />
-        <Route exact path='/badmin' component={Admin} />
-        <Route exact path='*' component={PageNotFound} />
-      </Switch>
-    </BrowserRouter>
+    <React.Suspense fallback={<Spinner />}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={() => <b>/</b>} />
+          <Route exact path='/fuq/:id' component={Fuq} />
+          <Route exact path='/badmin' component={Admin} />
+          <Route exact path='*' component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    </React.Suspense>
   );
 };
 
