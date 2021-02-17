d8dc191c64d1094bb94e404ae9389542aed924a1
MarkAnn <tiberi64@yandex.ru>
2021-02-17 16:47:33 +0700

    test

--- src/api/config.ts
+++ src/api/config.ts
@@ -12,7 +12,7 @@ import { ConfigType } from 'config/model';
  * Fetches the config file with basename
  */
 export const useConfig = () => {
-  const a = 'Test';
+  const b = 'Test'; 
 
   return useQuery<unknown, AxiosError, ConfigType>("Get configuration file", () => call('/config.json', 'GET'));
 };
