9b2eda06ce2e3b879ac360b835446e5c78d5e873
RipDevil <tiberi64@yandex.ru>
2021-03-01 14:14:52 +0700

    :dvd: FuqCard tests

---
+++ src/__tests__/components/fuqCard.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { FuqCard } from 'components/common';
+
+test('FuqCard should be rendered', () => {
+  const { getByText, container } = render(<FuqCard title="Some title" text="Some text" />);
+  const [elementWithTitle, elementWithText] = [getByText('Some title'), getByText('Some text')];
+
+  expect(container).toMatchSnapshot();
+  expect(elementWithTitle).toBeInTheDocument();
+  expect(elementWithText).toBeInTheDocument();
+});
