54706c0531ba643a2bf2a2cc4ad4836d58c34351
RipDevil <tiberi64@yandex.ru>
2021-03-01 17:26:20 +0700

    :dvd: deleted service worker

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,6 @@ import App from 'app';
 import { Credits } from 'components/common';
 import { ConfigWrapper } from 'config';
 
-import * as serviceWorker from './serviceWorker';
-
 const client = new QueryClient({
   defaultOptions: {
     queries: {
@@ -32,8 +30,3 @@ ReactDOM.render(
   </QueryClientProvider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
