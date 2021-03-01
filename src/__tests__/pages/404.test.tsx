7391d04c4f56dabb1b2ab6a5b091a42b11a44058
RipDevil <tiberi64@yandex.ru>
2021-03-01 16:14:01 +0700

    :dvd: 404 tests

---
+++ src/__tests__/pages/404.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import PageNotFound from 'pages/404/page';
+
+test('404 should be rendered', () => {
+  const { container } = render(<PageNotFound />);
+  expect(container).toMatchSnapshot();
+});
