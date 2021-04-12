09f05744a56ece00206a572b8035f36c9783b79f
RipDevil <tiberi64@yandex.ru>
2021-04-12 17:26:24 +0700

    :dvd: changed how the Credits comp is look like

--- src/components/common/credits/credits.styled.tsx
+++ src/components/common/credits/credits.styled.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const CreditsComponent = styled.div`
   position: fixed;
-  top: 0;
-  left: 10;
+  bottom: 10px;
+  right: 10px;
   z-index: 1001;
   overflow: hidden;
   font-size: 10px;
