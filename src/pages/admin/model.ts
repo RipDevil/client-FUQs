eb8bc376e0c16c374c92c36279c50aa66ed506b6
RipDevil <tiberi64@yandex.ru>
2021-04-15 16:48:32 +0700

    :fire: deleted the password field from the UserType

--- src/pages/admin/model.ts
+++ src/pages/admin/model.ts
@@ -3,6 +3,5 @@ import { FuqType } from 'pages/fuq/model';
 export interface UserType {
   login: string;
   fuqs: FuqType[];
-  password: string;
   deleted: boolean;
 } // password is presented? lol?
