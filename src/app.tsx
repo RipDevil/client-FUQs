03aef54f7f63985fb30513ce4d3d2a0c008522db
RipDevil <tiberi64@yandex.ru>
2021-05-27 11:41:21 +0700

    :hammer: moved antd message config

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { Switch, HashRouter } from 'react-router-dom';
+import { message } from 'antd';
 
 import { Routes } from 'routes';
 import { Portal } from 'components/admin';
+
+message.config({
+  duration: 1,
+});
 
 const App: React.FC = () => {
   return (
